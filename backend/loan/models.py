781fc0512b82991b794c7177f00af04ca488bb8e
aadharkansal <aadharkansal.cer18@iitbhu.ac.in>
2022-05-07 22:53:14 +0530

    bank detail model added

--- backend/loan/models.py
+++ backend/loan/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.core.validators import MaxLengthValidator, MinLengthValidator
 
 # Create your models here.
+
+class UserBankData(models.Model):
+    account_no = models.IntegerField(validators=[MaxLengthValidator(10),MinLengthValidator(10)])
+    bank_name = models.CharField(max_length=255)
+    bank_code = models.IntegerField(validators=[MaxLengthValidator(4),MinLengthValidator(4)])
+    bank_address = models.TextField()
+
+    def __str__(self):
+        return self.account_no
