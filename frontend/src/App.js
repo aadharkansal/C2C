94574ab9efc65755fa0dfb80221d17e1d54b5f6b
tanmay-raj-29 <tanmayraj29.20@gmail.com>
2022-05-09 19:03:24 +0530

    add axios and links in react router

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,19 +1,26 @@
 import { Route, Routes } from "react-router-dom";
 import './App.css';
+import Dashboard from "./components/pages/Dashboard";
 import Login from './components/pages/Login';
 import NotFound from './components/pages/NotFound';
 import Register from './components/pages/Register';
+import Layout from './components/layout/Layout';
+import LoanRequests from "./components/pages/LoanRequests";
+import Account from "./components/pages/Account";
 
 function App() {
   return (
     <div className="App">
-      <Routes>
-        <Route path="/" element={<p>HOME PAGE</p>} />
-        <Route path="/terms" element={<p>Terms & Conditions</p>} />
-        <Route path="login" element={<Login />} />
-        <Route path="register" element={<Register />} />
-        <Route path="*" element={<NotFound />} />
-      </Routes>
+      <Layout>
+        <Routes>
+          <Route path="/" element={<Dashboard />} />
+          <Route path="/loans" element={<LoanRequests />} />
+          <Route path="/account" element={<Account />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/*" element={<NotFound />} />
+        </Routes>
+      </Layout>
     </div>
   );
 }
---
+++ frontend/src/components/pages/Dashboard.js
@@ -0,0 +1,7 @@
+import Layout from "../layout/Layout";
+
+const Dashboard = () => {
+    return <h1>RAJ</h1>
+};
+
+export default Dashboard;
